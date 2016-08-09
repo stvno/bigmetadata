19db27b3b3bd5a4c16d5b68afed031f7400a2ef3
Steven M. Ottens <github@minst.net>
2016-08-09 18:31:46 -0400

    Dutch statistics 2014 url

---
+++ tasks/nl/cbs.py
@@ -0,0 +1,3 @@
+#https://www.cbs.nl/nl-nl/maatwerk/2015/48/kerncijfers-wijken-en-buurten-2014
+#https://www.cbs.nl/nl-nl/dossier/nederland-regionaal/geografische%20data/wijk-en-buurtkaart-2014
+#http://download.cbs.nl/regionale-kaarten/shape-2014-versie-2-0.zip
